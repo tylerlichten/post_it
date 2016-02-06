943a1ce5b8500e016039261b375017d275de7811
Tyler Lichten <tlich10@gmail.com>
2016-02-06 12:33:38 -0500

    added ImageMagick and Paperclip, updated posts views

--- post_it/app/controllers/posts_controller.rb
+++ post_it/app/controllers/posts_controller.rb
@@ -73,7 +73,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:description)
+      params.require(:post).permit(:description, :image)
     end
 
   def correct_user
--- post_it/db/schema.rb
+++ post_it/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206062223) do
+ActiveRecord::Schema.define(version: 20160206171621) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"

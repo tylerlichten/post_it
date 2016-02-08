200e4297eed43dc25c84355e5388470d43e2249d
Tyler Lichten <tlich10@gmail.com>
2016-02-07 22:56:04 -0500

    added pagination, changed posts to index page, made site mobile friendly

--- post_it/app/controllers/posts_controller.rb
+++ post_it/app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 25)
   end
 
   # GET /posts/1
--- post_it/config/routes.rb
+++ post_it/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
-  root 'home#index'
+  root 'posts#index'
   get 'home/about'
   get 'home/contact'
   get 'home/faq'
